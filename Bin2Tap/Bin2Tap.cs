e4329b7e417df19e452525a3c2b1c4c7763f30a5
Juan Antonio Rubio García <juanantonio.rubiogarcia@outlook.com>
2024-10-12 13:07:48 +0200

    Modificación en Bin2Tap a petición de Juntelart

--- Bin2Tap/Bin2Tap.cs
+++ Bin2Tap/Bin2Tap.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace ZxFilesConverter
 {
@@ -8,7 +11,7 @@ namespace ZxFilesConverter
     /// </summary>
     internal class Bin2Tap
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             if (args == null || args.Length == 0)
             {
@@ -16,28 +19,81 @@ namespace ZxFilesConverter
             }
             else
             {
+                List<string> inputPaths = new List<string>();
+                List<string> outputPaths = new List<string>();
+                List<string> blockNames = new List<string>();
+                List<int> addresses = new List<int>();
+                List<int> blockTypes = new List<int>();
+
                 foreach (string arg in args)
                 {
-                    string name = Path.GetFileName(arg).Replace(Path.GetExtension(arg), "");
-                    string path = Path.GetFullPath(arg).Replace(Path.GetExtension(arg), ".tap");
+                    if (arg.Length < 2)
+                    {
+                        Console.WriteLine($"Bin2Tap: fatal error, param: {arg}");
+
+                        return -1;
+                    }
+
+                    string param = arg.ToLower().Substring(0, 2);
+                    string value = arg.Substring(2);
+                    int valueInt = 0;
+
+                    switch (param)
+                    {
+                        case "":
+                        case "-i":
+                            inputPaths.Add(arg);
+                            break;
+                        case "-a":
+                            if (!int.TryParse(value, out valueInt))
+                            {
+                                Console.WriteLine($"Bin2Tap: fatal error, param: {arg}");
+                            }
+                            addresses.Add(valueInt);
+                            break;
+                        case "-o":
+                            outputPaths.Add(param);
+                            break;
+                        case "p":
+                            if (!int.TryParse(value, out valueInt))
+                            {
+                                Console.WriteLine($"Bin2Tap: fatal error, param: {arg}");
+                            }
+                            blockTypes.Add(valueInt == 0 ? 0 : 3);
+                            break;
+                        case "-s":
+                            blockNames.Add(param);
+                            break;
+                    }
+                }
+
+                for (int i = 0; i < inputPaths.Count; i++)
+                {
+                    string inputPath = inputPaths[i];
+                    string outputPath = outputPaths.Count > i ? outputPaths[i] : $"{Path.GetFileNameWithoutExtension(inputPaths[i])}.tap";
+                    int address = addresses.Count > i ? addresses[i] : 0;
+                    int blockType = blockTypes.Count > i ? blockTypes[i] : 3;
+                    string blockName = blockNames.Count > i ? blockNames[i] : blockType == 0 ? "PROGRAM" : "BYTES";
                     byte[] buffer;
 
-                    using (FileStream r = new FileStream(arg, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (FileStream r = new FileStream(inputPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                     {
                         buffer = new byte[r.Length];
                         r.Read(buffer, 0, buffer.Length);
                         r.Close();
                     }
 
-                    buffer = ZxFileConverter.Bin2Tap(buffer, name);
-                    
-                    using (FileStream w = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None, buffer.Length))
+                    buffer = ZxFileConverter.Bin2Tap(buffer, blockName, address, blockType);
+
+                    using (FileStream w = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.None, buffer.Length))
                     {
                         w.Write(buffer, 0, buffer.Length);
                         w.Close();
                     }
                 }
             }
+
+            return 0;
         }
     }
 }
